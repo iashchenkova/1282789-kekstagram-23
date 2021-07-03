652dbc2cdf96dd7a4a1d644c807d5349fe8920e2
Ященкова Анна <iashchenkova.a@gmail.com>
2021-07-03 18:44:33 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
+const ALL_PHOTO_DESCRIBTIONS = 25;
+
 function getRandomNumber(min, max) {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
@@ -28,11 +30,11 @@ const createPhotoDescription = () => {
   const randomAvatarIndex = Math.floor(Math.random() * 6) + 1;
   const likes = Math.floor(Math.random() * 200) + 15;
   const id = randomNumber;
-  const url =  'photos/ ${randomNumber} jpg'; 
+  const url = 'photos/${randomNumber}.jpg'; 
   const  description = 'Это фотография';
   const comments = {
     id: randomNumber,
-    avatar: 'img/avatar' + randomAvatarIndex + '.svg',
+    avatar: 'img/avatar/${randomAvatarIndex}.svg',
     message: getRandomNumber(1, MESSAGES.length),
     name: getRandomNumber(1, NAMES.length),
   };
@@ -45,6 +47,5 @@ const createPhotoDescription = () => {
   };
 };
 
-const ALL_PHOTO_DESCRIBTIONS = 25;
 const allPhotos = new Array(ALL_PHOTO_DESCRIBTIONS).fill(null).map(()=> createPhotoDescription());
 console.log(allPhotos());
